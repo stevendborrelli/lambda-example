58bd20a9a666c2c43c99b32ebf258a09d511416e
Steven Borrelli <steve@borrelli.org>
2023-05-31 17:44:38 +0100

    update test and config
    Signed-off-by: Steven Borrelli <steve@borrelli.org>

--- test/setup.sh
+++ test/setup.sh
@@ -2,16 +2,18 @@
 set -aeuo pipefail
 
 echo "Running setup.sh"
-echo "Waiting until configuration package is healthy/installed..."
-${KUBECTL} wait configuration.pkg platform-ref-lambda --for=condition=Healthy --timeout 5m
+echo "Waiting until configuration package is installed..."
 ${KUBECTL} wait configuration.pkg platform-ref-lambda --for=condition=Installed --timeout 5m
+echo "Waiting until configuration package is healthy..."
+${KUBECTL} wait configuration.pkg platform-ref-lambda --for=condition=Healthy --timeout 5m
+
 
 echo "Creating cloud credential secret..."
 ${KUBECTL} -n upbound-system create secret generic aws-creds --from-literal=credentials="${UPTEST_CLOUD_CREDENTIALS}" \
     --dry-run=client -o yaml | ${KUBECTL} apply -f -
 
-echo "Waiting until provider-aws is healthy..."
-${KUBECTL} wait provider.pkg upbound-provider-aws --for condition=Healthy --timeout 5m
+#echo "Waiting until provider-aws is healthy..."
+#${KUBECTL} wait provider.pkg upbound-provider-aws --for condition=Healthy --timeout 5m
 
 echo "Waiting for all pods to come online..."
 "${KUBECTL}" -n upbound-system wait --for=condition=Available deployment --all --timeout=5m
